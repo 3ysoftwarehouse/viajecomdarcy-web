e207fb7ade8cea3597b12fad33678ddea7ce59af
luiz carlos <lcpojr@hotmail.com>
2016-08-26 15:51:48 -0300

    CRUD moeda

---
+++ apps/moeda/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Moeda(models.Model):
+	id_moeda = models.AutoField(primary_key=True)
+	moeda_desc = models.CharField(max_length=15)
+	moeda_cambio = models.DecimalField(max_digits=4, decimal_places=3)
+	moeda_ultimaatualizacao = models.DateTimeField(auto_now_add=True)
+	moeda_simbolo = models.CharField(max_length=2)
+	
+	def __str__(self):
+		return self.moeda_desc
+	def __unicode__(self):
+		return unicode(self.moeda_desc)
---
+++ apps/moeda/views.py
@@ -0,0 +1,63 @@
+#-*- coding: utf-8 -*-
+
+##################################################
+#				DJANGO IMPORTS                   #
+##################################################
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render,redirect
+from django.views.generic import CreateView, RedirectView, View, UpdateView, ListView, DetailView, DeleteView
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import (HttpResponse,
+                         HttpResponseForbidden,
+                         HttpResponseBadRequest)
+from django.forms import formset_factory
+##################################################
+
+
+
+##################################################
+#				CUSTOM IMPORTS                   #
+##################################################
+from .models import Moeda # MODELS
+from apps.default.views import JSONResponseMixin
+##################################################
+
+
+class MoedaRegister(JSONResponseMixin,CreateView):
+    model = Moeda
+    template_name = 'moeda/register.html'
+    fields = ['moeda_desc', 'moeda_cambio', 'moeda_simbolo']
+    success_url = reverse_lazy('moeda-list')
+
+
+
+class MoedaEdit(JSONResponseMixin,UpdateView):
+    model = Moeda
+    template_name = 'moeda/edit.html'
+    fields = ['moeda_desc', 'moeda_cambio', 'moeda_simbolo']
+    success_url = reverse_lazy('moeda-list')
+
+
+class MoedaList(JSONResponseMixin,ListView):
+	model = Moeda
+	template_name = 'moeda/list.html'
+
+	def get_context_data(self, **kwargs):
+		context = super(MoedaList, self).get_context_data(**kwargs)
+		return context
+
+
+class MoedaDetail(JSONResponseMixin,DetailView):
+	model = Moeda
+	template_name = 'moeda/detail.html'
+
+	def get_context_data(self, **kwargs):
+		context = super(UserDetail, self).get_context_data(**kwargs)
+		return context
+
+
+class MoedaDelete(JSONResponseMixin,DeleteView):
+	model = Moeda
+	success_url = reverse_lazy('moeda-list')
+	template_name = 'moeda/delete.html'
