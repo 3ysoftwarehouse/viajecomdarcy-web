0c7b6a75dcc6f7abf0511cf1ff230164c2489ecc
luiz carlos <lcpojr@hotmail.com>
2016-08-29 15:43:26 -0300

    Correções

--- apps/subclasses/usuario/employee/forms.py
+++ apps/subclasses/usuario/employee/forms.py
@@ -19,3 +19,9 @@ from apps.default.forms import UserRegisterForm
 class EmployeeRegisterForm(UserRegisterForm, forms.Form):
 
 	nickname = forms.CharField(label='Nickname:', max_length=20)
+
+	def __init__(self, *args, **kwargs):
+		super(EmployeeRegisterForm, self).__init__(*args, **kwargs)
+
+		self.fields['nickname'].widget.attrs['class'] = 'form-control'
+		self.fields['nickname'].widget.attrs['placeholder'] = 'Digite o nickname'
