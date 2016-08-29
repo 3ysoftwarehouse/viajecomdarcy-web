bf4b0b9eac930683df6a8e2f010536a3aebe2628
luiz carlos <lcpojr@hotmail.com>
2016-08-29 11:07:15 -0300

    CRUD acomodacao

---
+++ apps/acomodacao/views.py
@@ -0,0 +1,63 @@
+#-*- coding: utf-8 -*-
+
+##################################################
+#				DJANGO IMPORTS                   #
+##################################################
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render,redirect
+from django.views.generic import CreateView, RedirectView, View, UpdateView, ListView, DetailView, DeleteView
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import (HttpResponse,
+                         HttpResponseForbidden,
+                         HttpResponseBadRequest)
+from django.forms import formset_factory
+##################################################
+
+
+
+##################################################
+#				CUSTOM IMPORTS                   #
+##################################################
+from .models import Acomodacao # MODELS
+from apps.default.views import JSONResponseMixin
+##################################################
+
+
+class AcomodacaoRegister(JSONResponseMixin,CreateView):
+    model = Acomodacao
+    template_name = 'acomodacao/register.html'
+    fields = ['acomodacao_desc']
+    success_url = reverse_lazy('acomodacao-list')
+
+
+
+class AcomodacaoEdit(JSONResponseMixin,UpdateView):
+    model = Acomodacao
+    template_name = 'acomodacao/edit.html'
+    fields = ['acomodacao_desc']
+    success_url = reverse_lazy('acomodacao-list')
+
+
+class AcomodacaoList(JSONResponseMixin,ListView):
+	model = Acomodacao
+	template_name = 'acomodacao/list.html'
+
+	def get_context_data(self, **kwargs):
+		context = super(AcomodacaoList, self).get_context_data(**kwargs)
+		return context
+
+
+class AcomodacaoDetail(JSONResponseMixin,DetailView):
+	model = Acomodacao
+	template_name = 'acomodacao/detail.html'
+
+	def get_context_data(self, **kwargs):
+		context = super(AcomodacaoDetail, self).get_context_data(**kwargs)
+		return context
+
+
+class AcomodacaoDelete(JSONResponseMixin,DeleteView):
+	model = Acomodacao
+	success_url = reverse_lazy('acomodacao-list')
+	template_name = 'acomodacao/delete.html'
