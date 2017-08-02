3b1f6ecadfecf97a46d3fd54cf3454ab9e238635
ytalokaio <ytalo@3ysoftwarehouse.com.br>
2017-08-02 11:42:45 -0300

    Criação do ap de gestão de LEAD

---
+++ apps/gestao_lead/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from apps.subclasses.empresa.escola.models import Escola
+
+# Create your models here.
+
+class Prospect(models.Model):
+    nome_completo = models.CharField(max_length=400)
+    email = models.EmailField(null=True, blank=True)
+    telefone = models.CharField(max_length=100, null=True)
+    escola = models.ForeignKey(Escola, on_delete=models.DO_NOTHING, null=True, blank=True)
+    observacao = models.TextField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome_completo
